640be785e3bf0507810fbb1086638e1c8811bd5f
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-19 07:55:42 +0200

    Create ViewModel CampComments

---
+++ Models/CampCommentsVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampplaceTest1.Models
+{
+    public class CampCommentsVM
+    {
+        public Camp Camp { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+}
