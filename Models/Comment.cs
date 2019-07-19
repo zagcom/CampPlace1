a6e155c5e627da690f48caaf1e6c89d7372c4bf8
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-19 18:05:27 +0200

    Comment form

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace CampplaceTest1.Models
         public int Id { get; set; }
         public int CampId { get; set; }
         public Camp Camp { get; set; }
+        [Display (Name ="Treść Komentarza")]
         public string Content { get; set; }
         public string UserName { get; set; }
         public DateTime Added { get; set; }
