bb5898b72855277e826ae56e801da4ba4a4e6b43
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-19 22:04:58 +0200

    Comment Index

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -9,12 +9,16 @@ namespace CampplaceTest1.Models
     public class Comment
     {
         public int Id { get; set; }
+        [Display(Name = "Obóz")]
         public int CampId { get; set; }
+        [Display(Name = "Obóz")]
         public Camp Camp { get; set; }
         [Display (Name ="Treść Komentarza")]
         public string Content { get; set; }
+        [Display(Name = "Autor")]
         public string UserName { get; set; }
         public string UserId { get; set; }
+        [Display(Name = "Data publikacji")]
         public DateTime Added { get; set; }
     }
 }
