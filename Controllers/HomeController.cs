51fdb06f9e07115647714b317af84c06fd06c1d2
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-23 22:54:48 +0200

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace CampplaceTest1.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Baza miejsc obozowych";
 
             return View();
         }
